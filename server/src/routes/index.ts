e31b65b20fe2667ed6726c675be3db6de2f5da07
Jehee <59410707+nuelvations@users.noreply.github.com>
2026-02-14 01:42:18 +0000

    rename main route entry

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import userRoutes from "./user.routes";
-import questionRoutes from "./questions.routes";
-import answerRoutes from "./answers.routes";
-import scoreRoutes from "./scores.routes";
+import userRoutes from "./user.routes.ts";
+import questionRoutes from "./questions.routes.ts";
+import answerRoutes from "./answers.routes.ts";
+import scoreRoutes from "./scores.routes.ts";
 
 const router = Router();
 
